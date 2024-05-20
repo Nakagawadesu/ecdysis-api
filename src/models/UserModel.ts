64790f3e2c7fe68a7e336992114f794c89956d6a
Nakagawadesu <nukitaro@outlook.com>
2024-05-20 16:00:38 -0300

    Add UserModel class for user management

---
+++ src/models/UserModel.ts
@@ -0,0 +1,57 @@
+import Logger from "../helpers/Logger";
+import {
+  OptionalId,
+  ObjectId,
+  Collection,
+  MongoClient,
+  Document,
+} from "mongodb";
+import Database from "../config/Database";
+import { UserType, AccountData, UserMetricsType } from "../types/UserType";
+import { resolve } from "path";
+const log = new Logger();
+
+class UserModel {
+  private client!: MongoClient;
+  private collection: Collection<Document>;
+  private databaseName = process.env.MONGO_DB_NAME;
+  private collectionName = "users";
+
+  constructor() {
+    const databaseInstance = Database.getInstance();
+    if (databaseInstance) {
+      this.client = databaseInstance.getClient();
+    }
+    this.client.connect();
+    this.collection = this.client
+      .db(this.databaseName)
+      .collection(this.collectionName);
+  }
+
+  private connectToCollection = async () => {
+    try {
+      this.client = await this.client.connect();
+      log.info(`Connected to database ${this.databaseName}`);
+    } catch (error) {
+      log.error(`error connecting to database ${this.databaseName}`);
+      throw error;
+    }
+    try {
+      this.collection = this.client
+        .db(this.databaseName)
+        .collection(this.collectionName);
+      log.info(`Connected to collection ${this.collectionName}`);
+    } catch {
+      log.error(`error connecting to collection ${this.collectionName}`);
+    }
+  };
+
+  public createUser = async (user: UserType) => {
+    log.group(`Create User Controller`);
+    try {
+      const response = await this.collection.insertOne(user);
+    } catch {}
+  };
+}
+
+export default UserModel;
