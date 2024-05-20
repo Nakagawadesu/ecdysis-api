789bcfbd5ad2674c1d33c56ce54717ace8d91860
Nakagawadesu <nukitaro@outlook.com>
2024-05-20 16:00:50 -0300

    Refactor ReportType interface in src/types/ReportType.ts

--- src/types/ReportType.ts
+++ src/types/ReportType.ts
@@ -1,15 +1,13 @@
 import { ObjectId } from "mongodb";
 import { string } from "yargs";
 
-type ReportType = {
+export type ReportType = {
   _id?: ObjectId;
   user: ObjectId;
   reportType: "skinQuality" | "skinHealth";
   createdAt: Date;
   title?: string;
   description?: string;
-  pimlesScore?: number;
-  wrikelsScore?: number;
   greaseScore?: number;
   deseaseClass?: ObjectId;
   confidence?: number;
