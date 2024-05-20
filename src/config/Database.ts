4af2f62b96660c9273083ca83d153a6647771639
Nakagawadesu <nukitaro@outlook.com>
2024-05-20 16:00:34 -0300

    Refactor Database class constructor and add getClient method

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -8,29 +8,38 @@ const log = new Logger();
 
 class Database {
   private static instance: Database;
-  private client: mongodb.MongoClient;
-  private databaseName: string;
+  private client!: mongodb.MongoClient;
+  private databaseName = process.env.MONGO_DB_NAME ?? "";
   private db: mongodb.Db | undefined;
 
-  private constructor() {
-    this.databaseName = process.env.MONGO_DB_NAME ?? "";
-    this.client = new MongoClient(process.env.MONGO_URI ?? "", {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
+  public constructor() {
+    try {
+      this.databaseName = process.env.MONGO_DB_NAME ?? "";
+      this.client = new MongoClient(process.env.MONGO_STRING ?? "", {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
+      log.success("MongoClient initialized");
+    } catch (err) {
+      log.error("Error in Database constructor: ", err);
+      log.info("MongoClient initialized");
+      log.info(
+        `Database name: ${process.env.MONGO_DB_NAME}, Connection string: ${process.env.MONGO_STRING}`
+      );
+    }
   }
 
-  public static getInstance(): Database | null {
+  static getInstance(): Database | null {
     try {
       if (!Database.instance) {
         Database.instance = new Database();
 
         log.info("Database instance created");
       }
-
+      log.success("Database instance returned");
       return Database.instance;
     } catch (err) {
       log.error("Error in Database.getInstance: ", err);
@@ -53,7 +62,8 @@ class Database {
     log.info("Database connection closed");
   }
 
-  getCollection(name: string) {
-    return this.db!.collection(name);
+  getClient(): MongoClient {
+    return this.client;
   }
 }
+export default Database;
