76a3086927125e1ed7ee5b453ba3ec17e7f707ea
Nakagawadesu <nukitaro@outlook.com>
2024-05-30 21:06:49 -0300

    Add field filtering and error handling to Create User Controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,8 @@ import UserModel from "../models/UserModel";
 import { Request, Response, NextFunction } from "express";
 import Logger from "../helpers/Logger";
 import { UserType } from "../types/UserType";
+import Filter from "../helpers/Filter";
+import { userFields } from "../helpers/Filter";
 const log = new Logger();
 class UserController {
   private userModel!: UserModel;
@@ -15,11 +17,47 @@ class UserController {
     next: NextFunction
   ) => {
     log.info("Create User Controller");
-    const user: UserType = req.body;
-    log.info("User data: ", user);
+    const body = req.body;
+    const missingFields = Filter.filterFields(userFields, body);
+    if (missingFields) {
+      req.statusCode = 400;
+      req.statusMessage = `${
+        missingFields.length > 1
+          ? "existem campos faltando :"
+          : "existe um campo faltando :"
+      } 
+    ${missingFields.join(", ")}`;
+      log.groupEnd();
+      next();
+    }
+    const user: UserType = {
+      name: body.name,
+      accountData: {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      },
+      email: body.email,
+      password: body.password,
+    };
     try {
-      await this.userModel.createUser(user);
-    } catch {}
+      const res = await this.userModel.createUser(user);
+      if (!res) {
+        req.statusCode = 500;
+        req.statusMessage = "Error creating user";
+        log.groupEnd();
+        next();
+      }
+      req.statusCode = 200;
+      req.statusMessage = "User created successfully";
+      log.groupEnd();
+      next();
+    } catch {
+      req.statusCode = 500;
+      req.statusMessage = "Error creating user";
+      log.groupEnd();
+      next();
+    }
   };
 }
 
