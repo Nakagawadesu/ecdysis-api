57be65893b7c3a5ef85476b2e0277106b1acf4df
Nakagawadesu <nukitaro@outlook.com>
2024-05-30 21:07:04 -0300

    Remove unused properties from UserType

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -2,11 +2,8 @@ import { ObjectId } from "mongodb";
 export type UserType = {
   _id?: ObjectId;
   accountData: AccountData;
-  skinQualityReports: Array<ObjectId>;
-  skinQualityReportsCount?: number;
   SkinHealthReports: Array<ObjectId>;
   SkinHealthReportsCount?: number;
-  Metrics?: UserMetricsType;
 };
 export type AccountData = {
   username: string;
@@ -19,14 +16,5 @@ export type AccountData = {
 };
 export type UserMetricsType = {
   Age?: number;
-  mediumPimplesScore: number;
-  mediumwrikelsScore: number;
-  mediumGreaseScore: number;
-  HighestPimplesScore: number;
-  HighestWrikelsScore: number;
-  HighestGreaseScore: number;
-  LowestPimplesScore: number;
-  LowestWrikelsScore: number;
-  LowestGreaseScore: number;
   HealthIssues: Array<string>;
 };
