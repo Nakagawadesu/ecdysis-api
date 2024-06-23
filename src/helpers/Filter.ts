a4a6d757bbf7fd837fc3d044b3f82acb04095daf
Nakagawadesu <nukitaro@outlook.com>
2024-06-23 12:24:56 -0300

    ♻️ - refactor: Turn Filter into a Class according to SOLID principles

--- src/helpers/Filter.ts
+++ src/helpers/Filter.ts
@@ -1,15 +1,51 @@
+import HttpStatusCode from "../utils/HttpStatusCode";
+
 class Filter {
-  static filterFields(fieldList: string[], Obj: Object) {
+  static getMissingFields(fieldList: string[], Obj: Object) {
     const missingFields: string[] = [];
 
-    Object.keys(Obj).forEach((key: string) => {
-      if (!fieldList.includes(key)) {
+    fieldList.forEach((key: string) => {
+      if (!Object.keys(Obj).includes(key)) {
         missingFields.push(key);
       }
     });
 
     return missingFields.length > 0 ? missingFields : null;
   }
+  static validateFields = (fields: string[], data: Record<string, any>) => {
+    // Check if data is provided
+    if (!data) {
+      return {
+        status: HttpStatusCode.BAD_REQUEST,
+        message: `Dados não fornecidos`,
+      };
+    }
+
+    const missingFields = this.getMissingFields(fields, data);
+    // Return null if everything is fine
+    if (!missingFields) return null;
+
+    const missingFieldsStr = missingFields.join(", ");
+
+    if (missingFields.length > 0) {
+      return {
+        status: HttpStatusCode.BAD_REQUEST,
+        message: `${
+          missingFields.length > 1
+            ? "existem campos faltando :"
+            : "existe um campo faltando :"
+        } 
+              ${missingFields.join(", ")}`,
+        log: `Missing fields : ${missingFields}`,
+      };
+    }
+  };
+
+  static trimEmail = (data: string) => {
+    const trimmedEmail = data.toLowerCase().trim();
+    console.log(`trimmedEmail: ${trimmedEmail}`);
+    return trimmedEmail;
+  };
 }
-export const userFields = ["name", "email", "password"];
+export const userFields = ["name", "email", "password", "confirmPassword"];
 export default Filter;
