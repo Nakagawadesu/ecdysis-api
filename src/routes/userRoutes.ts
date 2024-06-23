ac709e4bb03a487bfc0defa0e602851d6ecee7a6
Nakagawadesu <nukitaro@outlook.com>
2024-06-23 12:29:07 -0300

    ♻️ - refactor: Refactor Routing process to make aproppriate use of Singleton Database COnnection

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,24 @@
 import express from "express";
-import UserController from "../controllers/UserController";
+import RegistryController from "../controllers/User/RegistryController";
+import GeneralUserController from "../controllers/User/GeneralUserController";
+import LoginController from "../controllers/User/LoginController";
+import UserModel from "../models/User/UserModel";
 
 const router = express.Router();
 
-const userController = new UserController();
-router.post("/register", userController.createUser);
+const userModelInstace = new UserModel();
+
+const registryController = new RegistryController(userModelInstace);
+const userController = new GeneralUserController();
+const loginController = new LoginController(userModelInstace);
+
+router
+  .post("/auth/register", registryController.RegisterUser)
+  .post("/auth/login", loginController.LoginUser)
+  .get("/auth/confirm/email", registryController.confirmEmail)
+  .post("/auth/send/confirm/email", registryController.sendVerificationEmail);
+// .get("/user", userController.getProfile)
+// .put("/user", userController.updateProfile)
+// .delete("/user", userController.deleteProfile);
 
 export default router;
