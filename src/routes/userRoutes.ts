8d5395714172c61278e7767c0a80f54768a963e3
Nakagawadesu <nukitaro@outlook.com>
2024-06-24 22:04:23 -0300

    Add image processing functionality to user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,12 +2,18 @@ import express from "express";
 import RegistryController from "../controllers/User/RegistryController";
 import GeneralUserController from "../controllers/User/GeneralUserController";
 import LoginController from "../controllers/User/LoginController";
+import ImageProcessingController from "../controllers/User/ImageProcessingController";
 import UserModel from "../models/User/UserModel";
+
+import multer from "multer";
+
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
 const userModelInstace = new UserModel();
 
+const imageProcessingController = new ImageProcessingController();
 const registryController = new RegistryController(userModelInstace);
 const userController = new GeneralUserController();
 const loginController = new LoginController(userModelInstace);
@@ -16,8 +22,13 @@ router
   .post("/auth/register", registryController.RegisterUser)
   .post("/auth/login", loginController.LoginUser)
   .get("/auth/confirm/email", registryController.confirmEmail)
-  .post("/auth/send/confirm/email", registryController.sendVerificationEmail);
-// .get("/user", userController.getProfile)
+  .post("/auth/send/confirm/email", registryController.sendVerificationEmail)
+  .post(
+    "/process/image",
+    upload.single("image"),
+    imageProcessingController.processImage
+  );
+//  .get("/user", userController.getProfile)
 // .put("/user", userController.updateProfile)
 // .delete("/user", userController.deleteProfile);
 
