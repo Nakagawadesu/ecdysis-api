194d98848af6a3227a2e55eb00ad9499b9b2e79c
Nakagawadesu <nukitaro@outlook.com>
2024-07-01 08:50:50 -0300

    Add image classification functionality to ImageProcessingController

--- src/controllers/User/ImageProcessingController.ts
+++ src/controllers/User/ImageProcessingController.ts
@@ -2,24 +2,48 @@ import { Request, Response, NextFunction } from "express";
 import requestAssembler from "../../services/RequestAssembler";
 import Logger from "../../helpers/Logger";
 import HttpStatusCode from "../../utils/HttpStatusCode";
-const log = new Logger();
 import { ClassifyImage } from "../../services/ImageClassifier";
+import ModelLoader from "../../CNN/modelLoader";
+import { classesEnum } from "../../CNN/classes";
+type ClassIndex = keyof typeof classesEnum;
+const log = new Logger();
+const modelLoader = new ModelLoader();
+
 class ImageProcessingController {
   public async processImage(req: Request, res: Response, next: NextFunction) {
+    log.group("ImageProcessingController");
     try {
       console.log(req.file);
+      log.success(`req.body ${JSON.stringify(req.body)}  `);
       if (!req.file) {
         const payload = {
           status: HttpStatusCode.BAD_REQUEST,
           message: "Nenhum Arquivo enviado",
           log: "No file uploaded",
         };
+
+        log.groupEnd();
         return requestAssembler.assembleRequest(req, next, payload);
       }
-      ClassifyImage(req.file);
-      return res.status(HttpStatusCode.OK);
+      log.success(` req.file.path: ${req.file.path}`);
+      const result = await ClassifyImage(req.file.path, modelLoader);
+      log.success(`Result: ${JSON.stringify(result)}`);
+      const payload = {
+        status: HttpStatusCode.OK,
+        message: "Imagem processada com sucesso",
+        log: `Predicted class: ${classesEnum[result as ClassIndex]}`,
+        body: result,
+      };
+      log.groupEnd();
+      return requestAssembler.assembleRequest(req, next, payload);
     } catch (error) {
-      next(error);
+      log.groupEnd();
+      const payload = {
+        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        message: "Erro ao processar a imagem",
+        log: `Error: ${error}`,
+      };
+      return requestAssembler.assembleRequest(req, next, payload);
     }
   }
 }
