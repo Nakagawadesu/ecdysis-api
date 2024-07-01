7464a8029991168abc074fa4ac58f9923c0e914e
Nakagawadesu <nukitaro@outlook.com>
2024-07-01 08:50:24 -0300

    Refactor modelLoader.ts and add preProcessImage method

--- src/CNN/modelLoader.ts
+++ src/CNN/modelLoader.ts
@@ -1,19 +1,50 @@
 import fs from "fs";
 import * as tf from "@tensorflow/tfjs-node";
 import path from "path";
-import { URL } from "url";
-
-const modelPath = path.resolve(
-  "/home/matheus/Projects/ecdysis-api/src/CNN/modeloCNN.h5"
+import Logger from "../helpers/Logger";
+const log = new Logger();
+const modelDir = path.resolve(
+  "/home/matheus/Projects/ecdysis-api/src/CNN/tfjs_model"
 );
-export const loadModel = async (model: any | null) => {
-  if (!model) {
-    if (fs.existsSync(modelPath)) {
-      const modelUrl = new URL(`file://${modelPath}`);
-      model = await tf.loadLayersModel(modelUrl.toString());
-      return model;
-    } else {
-      throw new Error("Model path doesn't exist");
-    }
+class ModelLoader {
+  public model: tf.LayersModel | null = null;
+
+  public constructor() {
+    this.loadModel().then((model) => {
+      this.model = model;
+    });
   }
-};
+  public loadModel = async (
+    model: tf.LayersModel | null = null
+  ): Promise<tf.LayersModel> => {
+    const modelPath = path.join(modelDir, "model.json");
+
+    if (!model) {
+      if (fs.existsSync(modelPath)) {
+        try {
+          model = await tf.loadLayersModel(`file://${modelPath}`);
+          console.log("Model loaded successfully");
+          return model;
+        } catch (error) {
+          log.error(`Error loading model: ${error}`);
+        }
+      } else {
+        throw new Error("Model path doesn't exist");
+      }
+    }
+
+    // If model is already loaded, return it
+    return model!;
+  };
+  public preProcessImage = async (imagePath: string) => {
+    const imageBuffer = fs.readFileSync(imagePath);
+    const tensor = tf.node
+      .decodeImage(imageBuffer, 3)
+      .resizeNearestNeighbor([224, 224]) // Change the image size here
+      .expandDims()
+      .toFloat();
+    return tensor;
+  };
+}
+
+export default ModelLoader;
